2e6ef4adecd275c8062a66e7b845f3dc88100660
Abel <aherrera@wootbit.io>
2022-02-22 15:43:13 -0600

    create user view and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/home'
+  get 'users/:id', to: 'pages#user'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
