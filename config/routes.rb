0c7d27f381f53c43e1351573f3c94c91f5493af2
Abel <aherrera@wootbit.io>
2022-02-22 16:17:03 -0600

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'users/:id', to: 'pages#user'
-  get 'users/:id/posts', to: 'pages#posts'
-  get 'users/:id/posts/:postid', to: 'pages#post'
+  get 'users/', to: 'users#index'
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/:id/posts', to: 'posts#index', as: 'user_posts'
+  get 'users/:id/posts/:postid', to: 'posts#show', as: 'user_post'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#home"
+  root "users#index"
 end
