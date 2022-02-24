61d66d0ce95e93f1cccc0c5aa9958f13b53eea2d
Abel <aherrera@wootbit.io>
2022-02-24 15:13:41 -0600

    Add params

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users/', to: 'users#index'
   get 'users/:id', to: 'users#show', as: 'user'
   get 'users/:id/posts', to: 'posts#index', as: 'user_posts'
-  get 'users/:id/posts/:postid', to: 'posts#show', as: 'user_post'
+  get 'users/:id/posts/:post_id', to: 'posts#show', as: 'user_post'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
